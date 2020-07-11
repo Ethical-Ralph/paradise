cbf0ca444670ae941af757d7741d7f44dea65cb5
Ethical-Ralph <rakinola90@gmail.com>
2020-07-11 15:24:48 +0100

    new commit

--- src/components/CompetitonDetails.js
+++ src/components/CompetitonDetails.js
@@ -1,10 +1,34 @@
 import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux';
+import { getCompetition } from '../Redux/competitions/competitionAction'
 
-const CompetitionDetails = ({ match }) => {
-  const [ competitonId, setCompetionId ] = useState(null)
+const CompetitionDetails = ({ match, competition, getCompetition }) => {
+
   useEffect(() => {
-    setCompetionId(match.params)
-  })
+    const fetchData = async () => {
+      const { params: {id} } = match;
+      await getCompetition(id)
+    };
+    fetchData()
+  },[]) 
+
+  return (
+    <>
+    <div style={{color: 'white'}}>{JSON.stringify(competition)}</div>
+    </>
+  )
 }
 
-export default CompetitionDetails
+
+const mapStateToProps = (state) => {
+ return {
+   competition: state.competition.competition
+ }
+}
+
+const mapDispatchToProps = {
+  getCompetition
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompetitionDetails);
