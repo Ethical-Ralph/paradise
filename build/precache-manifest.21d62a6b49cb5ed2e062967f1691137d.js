ffe4f56131177e3e59519255e7833f492127eee4
Ethical-Ralph <rakinola90@gmail.com>
2020-07-31 21:56:51 +0100

    build

---
+++ build/precache-manifest.21d62a6b49cb5ed2e062967f1691137d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac7f09d6d15e9029e793ed3c60f968c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2616b717734f3e65e83",
+    "url": "/static/js/2.19b67505.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.19b67505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce59a2df34d8c3bb822",
+    "url": "/static/js/main.8d8aebe8.chunk.js"
+  },
+  {
+    "revision": "bd57f357dfb985a1078a",
+    "url": "/static/js/runtime-main.63a60423.js"
+  },
+  {
+    "revision": "361f155a2193a5b782492a5dd8c94adb",
+    "url": "/static/media/bottle.361f155a.jpg"
+  },
+  {
+    "revision": "829405743a0f9fa2319d96024de93863",
+    "url": "/static/media/facebook-brands.82940574.svg"
+  },
+  {
+    "revision": "aaabd1c24228814026547caae2b29e9b",
+    "url": "/static/media/palmtree.aaabd1c2.png"
+  },
+  {
+    "revision": "7a59ca51cac8981d970b9da297e2ad11",
+    "url": "/static/media/telegram-brands.7a59ca51.svg"
+  },
+  {
+    "revision": "ec04b8ee7d82307a7991e8ba0ec19df1",
+    "url": "/static/media/youtube-brands.ec04b8ee.svg"
+  }
+]);
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Carousel from "../components/Carousel";
 import HowItWorks from "../components/HowItWorks";
 import Competitions from "./Competitons";
 import Winners from "../components/Winners";
 import CustomerSatisfation from "../components/CustomerSatisfation";
 import GiftShop from "../components/GiftShop";
-import LiveDrawList from "../components/LiveDrawList";
+import * as tokenService from "../Utils/tokenService";
 
 const Homepage = () => {
+  useEffect(() => {
+    tokenService.removeToken();
+  }, []);
   return (
     <>
       <Carousel />
